e6f14b33352e8c3b9a9355f219b95ba5628318a5
Archit Bansal <archit.bansal.93@gmail.com>
2020-04-15 17:10:16 +0200

    Minor fixes. Added example notebooks for deep ensembles and mcdropout.

--- pybnn/__init__.py
+++ pybnn/__init__.py
@@ -2,5 +2,5 @@ from pybnn.dngo import DNGO
 from pybnn.bayesian_linear_regression import BayesianLinearRegression
 from pybnn.base_model import BaseModel
 from pybnn.mlp import MLP
-from mcdropout import MCDropout
-from deep_ensemble import DeepEnsemble
+from pybnn.mcdropout import MCDropout
+from pybnn.deep_ensemble import DeepEnsemble
