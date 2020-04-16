fe814d9afc18a9447d9852756c4455da4efa176d
Archit Bansal <archit.bansal.93@gmail.com>
2020-04-16 13:04:22 +0200

    Added module level logger to pybnn.

--- pybnn/__init__.py
+++ pybnn/__init__.py
@@ -3,4 +3,8 @@ from pybnn.bayesian_linear_regression import BayesianLinearRegression
 from pybnn.base_model import BaseModel
 from pybnn.mlp import MLP
 from pybnn.mcdropout import MCDropout
+from pybnn.mcbatchnorm import MCBatchNorm
 from pybnn.deep_ensemble import DeepEnsemble
+
+import logging
+logging.getLogger(__name__).addHandler(logging.StreamHandler())
