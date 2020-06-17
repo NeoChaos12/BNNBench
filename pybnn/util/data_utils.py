3a5a9514fc0711ab4a00a2078574ce1c10e18501
Archit Bansal <archit.bansal.93@gmail.com>
2020-06-17 22:59:23 +0200

    Added support for loading 4 new datasets.

--- pybnn/util/data_utils.py
+++ pybnn/util/data_utils.py
@@ -1,9 +1,17 @@
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_boston, fetch_openml
 
 dataloader_args = {
-    "boston": {"return_X_y": True}
+    "boston": {"return_X_y": True},
+    "concrete": {"name": "Concrete_Data", "return_X_y": True},
+    "energy": {"name": "energy-efficiency", "return_X_y": True},
+    "kin8nm": {"name": "kin8nm", "return_X_y": True},
+    "yacht": {"name": "yacht_hydrodynamics", "return_X_y": True},
 }
 
 dataloaders = {
-    "boston": load_boston
+    "boston": load_boston,
+    "concrete": fetch_openml,
+    "energy": fetch_openml,
+    "kin8nm": fetch_openml,
+    "yacht": fetch_openml,
 }
