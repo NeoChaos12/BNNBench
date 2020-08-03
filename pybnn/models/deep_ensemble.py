4562a4ada0d0d33ed080241633cf532d87afe8c3
Archit Bansal <archit.bansal.93@gmail.com>
2020-08-03 02:00:52 +0200

    Partially updated DeepEnsemble.

--- pybnn/models/deep_ensemble.py
+++ pybnn/models/deep_ensemble.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import numpy as np
+from typing import Union
 
 import torch
 import torch.nn as nn
@@ -12,57 +13,110 @@ from collections import namedtuple
 
 logger = logging.getLogger(__name__)
 
+
+class GaussianNLL(nn.Module):
+    r"""
+    Defines the negative log likelihood loss function used for training neural networks that output a tensor
+    [mean, variance], assumed to be approximating the mean and variance of a Gaussian Distribution.
+    """
+
+    def __init__(self):
+        super(GaussianNLL, self).__init__()
+
+    def forward(self, input: torch.Tensor, target: torch.Tensor):
+        # Assuming network outputs variance
+        std = input[:, 1].view(-1, 1) ** 0.5
+        std = nn.functional.softplus(std) + 10e-6
+        mu = input[:, 0].view(-1, 1)
+        n = torch.distributions.normal.Normal(mu, std)
+        loss = n.log_prob(target)
+        return -torch.mean(loss)
+
+
 class DeepEnsemble(MLP):
     """
     An ensemble of MLPs that treats the results from every MLP as an approximation of a Gaussian Distribution and
-    combines them accordingly.
+    combines them accordingly. Each such MLP is an instance of Learner.
     """
-    nlearners: int
 
-    # Add any new parameters needed exclusively by this model here
+    # Type hints for user-modifiable attributes go here
+    n_learners: int
+    # ------------------------------------
+
+    # Attributes that are not meant to be user-modifiable model parameters go here
+    # It is expected that any child classes will modify them as and when appropriate by overwriting them
+    _n_learners = 0
+    _learners: list
+    # ------------------------------------
+
+    # Add any new configurable model parameters needed exclusively by this model here
     __modelParamsDefaultDict = {
-        "nlearners": 5
+        "n_learners": 5
     }
     __modelParams = namedtuple("deepEnsembleModelParams", __modelParamsDefaultDict.keys(),
                                defaults=__modelParamsDefaultDict.values())
+    # ------------------------------------
 
-    # Combine the parameters used by this model with those of the MLP Model
+    # Combine the parameters used by this model with those of the Base Model
     modelParamsContainer = namedtuple(
         "allModelParams",
         tuple(__modelParams._fields_defaults.keys()) + tuple(MLP.modelParamsContainer._fields_defaults.keys()),
         defaults=tuple(__modelParams._fields_defaults.values()) +
                  tuple(MLP.modelParamsContainer._fields_defaults.values())
     )
+    # ------------------------------------
 
-    # Create a record of all default parameter values used to run this model, including the Base Model parameters
+    # Create a record of all default parameter values used to run this model, including the super class parameters
     _default_model_params = modelParamsContainer()
+    # ------------------------------------
 
-    def __init__(self, nlearners=_default_model_params.nlearners, **kwargs):
+
+    @property
+    def n_learners(self):
+        return self._n_learners
+
+
+    @n_learners.setter
+    def n_learners(self, val):
+        assert isinstance(val, int), f"Number of learners can only be set to an int, not {type(val)}."
+        assert val > 0, f"Number of learners must be positive."
+        if self._n_learners > val:
+            # If the number of learners is being reduced, just drop the learners at the tail end of the list
+            self._learners = self._learners[:val]
+        elif self._n_learners < val:
+            # If the number of learners is being increased, add only the required number of learners to the list
+            # Since the default value of _n_learners is 0, this condition will also be called when n_learners is set
+            # for the first time in __init__().
+            super_model_params = self.super_model_params
+            for _ in range(val - self._n_learners):
+                learner = MLP()
+                learner.model_params = super_model_params
+                self._learners.append(learner)
+        else:
+            # If the number of learners remains unchanged, don't really do anything
+            pass
+        self._n_learners = val
+
+    def __init__(self, n_learners=_default_model_params.n_learners, **kwargs):
         r"""
         Initialize a Deep Ensemble model.
-        Note that regardless of whether or not a different initial value was supplied, output_dims will always be set
-        to 2 and should not be changed. Similarly, the loss function used is set to
-        pybnn.models.deep_ensembles.GaussianNLL.
+
         Parameters
         ----------
-        nlearners: int
+        n_learners: int
             Number of base learners (i.e. MLPs) to use. Default is 5.
         kwargs: dict
             Other model parameters for MLP.
         """
         try:
-            model_params = kwargs.pop('model_params')
+            # We no longer support using this keyword argument to initialize a model
+            _ = kwargs.pop('model_params')
         except (KeyError, AttributeError):
-            self.nlearners = nlearners
             super(DeepEnsemble, self).__init__(**kwargs)
+            self.n_learners = n_learners
         else:
-            self.model_params = model_params
-
-        # Regardless of whether or not a different value was originally supplied, set output_dims to 2
-        self.output_dims = 2
-        # And set the loss function to GaussianNLL
-        self.loss_func = GaussianNLL()
-        self.learners = []
+            raise RuntimeError("Using model_params in the __init__ call is no longer supported. Create an object using "
+                               "default values first and then directly set the model_params attribute.")
 
         logger.info("Intialized Deep Ensemble model.")
         logger.debug(f"Intialized Deep Ensemble model parameters:\n{self.model_params}")
@@ -94,7 +148,6 @@ class DeepEnsemble(MLP):
             of individual learners after set intervals of training.
         """
 
-
         self.X = X
         self.y = y
 
@@ -107,7 +160,7 @@ class DeepEnsemble(MLP):
         start_time = time.time()
         mlp_params = self.super_model_params
         logger.debug("Generating learners using configuration:\n%s" % str(mlp_params))
-        self.learners = [MLP(model_params=mlp_params) for _ in range(self.nlearners)]
+        self.learners = [MLP(model_params=mlp_params) for _ in range(self.n_learners)]
 
         if conf.tblog:
             model_exp_name = conf.tb_exp_name
@@ -226,22 +279,3 @@ class DeepEnsemble(MLP):
             std *= self.y_std
 
         return mean, std
-
-
-class GaussianNLL(nn.Module):
-    r"""
-    Defines the negative log likelihood loss function used for training neural networks that output a tensor
-    [mean, variance], assumed to be approximating the mean and variance of a Gaussian Distribution.
-    """
-
-    def __init__(self):
-        super(GaussianNLL, self).__init__()
-
-    def forward(self, input, target):
-        # Assuming network outputs std
-        std = input[:, 1].view(-1, 1)
-        std = nn.functional.softplus(std) + 10e-6
-        mu = input[:, 0].view(-1, 1)
-        n = torch.distributions.normal.Normal(mu, std)
-        loss = n.log_prob(target)
-        return -torch.mean(loss)
