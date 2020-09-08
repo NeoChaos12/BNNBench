Fixed data normalization std clipping value.
Signed-off-by: Archit Bansal <archit.bansal.93@gmail.com>

pybnn/utils/normalization.py
@@ -21,7 +21,8 @@ def zero_mean_unit_var_normalization(X, mean=None, std=None):
     if mean is None:
         mean = np.mean(X, axis=0)
     if std is None:
-        std = np.clip(np.std(X, axis=0), a_min=1e-6, a_max=None)
+        std = np.std(X, axis=0)
+        std = np.where(np.isclose(std, 0.0, atol=1e-3), np.ones_like(std), std)
 
     X_normalized = (X - mean) / std
 