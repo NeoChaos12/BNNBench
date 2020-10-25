609ee30bc6222164b6f2c0328999d58b730b2ee7
Archit Bansal <archit.bansal.93@gmail.com>
2020-10-25 21:58:45 +0100

    UPDATED xgboost sampling script.
    Signed-off-by: Archit Bansal <archit.bansal.93@gmail.com>

--- pybnn/bin/xgboost_sampler.py
+++ pybnn/bin/xgboost_sampler.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--num_samples", type=int, default=1000)
     parser.add_argument("-x", "--runs_per_sample", type=int, default=10)
     parser.add_argument("-d", "--output_dir", type=str, default=".")
+    parser.add_argument("-e", "--extension", type=str, default="csv")
     args = parser.parse_args()
 
     task_id = args.task_id
@@ -31,20 +32,39 @@ if __name__ == "__main__":
     nsamples = args.num_samples
     runs_per_sample = args.runs_per_sample
     output_dir = Path(args.output_dir).expanduser().resolve()
+    extension = args.extension
 
-    data_file = output_dir / f"xgboost_{task_id}_rng{rng_seed}_data.csv"
-    header_file = output_dir / f"xgboost_{task_id}_rng{rng_seed}_headers.csv"
+    full_benchmark_name = f"xgboost_{task_id}_rng{rng_seed}"
+
+    data_file = output_dir /  f"{full_benchmark_name}_data.{extension}"
+    headers_file = output_dir / f"{full_benchmark_name}_headers.{extension}"
+    feature_ind_file = output_dir / f"{full_benchmark_name}_feature_indices.{extension}"
+    output_ind_file = output_dir / f"{full_benchmark_name}_output_indices.{extension}"
+    meta_ind_file = output_dir / f"{full_benchmark_name}_meta_indices.{extension}"
 
     benchmark = Bench(task_id=task_id, rng=rng_seed)
     total_rows = nsamples * runs_per_sample
     data = pd.DataFrame(data=None, index=list(range(runs_per_sample)), columns=cols)
     configs = cspace.sample_configuration(nsamples)
 
+    default_feature_header_indices = list(range(len(hypers)))
+    default_output_header_index = len(cols) - 2
+    default_meta_header_index = len(cols) - 1
+
     def write_data_to_file():
         data.to_csv(data_file, sep=" ", header=False, index=False, mode='a')
 
-    with open(header_file, "w") as fp:
+    with open(headers_file, 'w') as fp:
         fp.write("\n".join(data.columns.tolist()))
+
+    with open(feature_ind_file, 'w') as fp:
+        fp.write("\n".join(default_feature_header_indices))
+
+    with open(output_ind_file, 'w') as fp:
+        fp.write(default_output_header_index)
+
+    with open(meta_ind_file, 'w') as fp:
+        fp.write(default_meta_header_index)
 
     # How to interpret this iterator black magic: Generate a combined iterator from the chain of iterables
     # obtained by applying _repeat_ on every element of _configs_ for _nsamples_ repetitions.
