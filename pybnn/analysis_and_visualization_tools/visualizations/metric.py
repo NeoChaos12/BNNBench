eda97db6d0de7e212f92b920890c1e2d3ba4b25a
Archit Bansal <archit.bansal.93@gmail.com>
2020-12-16 01:27:16 +0100

    Minor clean-up and optimizations for metric visualizations.

--- pybnn/analysis_and_visualization_tools/visualizations/metric.py
+++ pybnn/analysis_and_visualization_tools/visualizations/metric.py
@@ -1,15 +1,10 @@
 """
-Contains the all visualizations for metric values supported by the package. Currently available visualizations are:
+Contains the all visualizations for metric dataframes supported by the package. Currently available visualizations are:
 
 'mean-std': A comparison of the mean and 1-sigma values of observed datapoints across the given indices. Accepts upto
 three index names, using the first index to generate in-plot labels across curves, the second index to generate subplot
 columns, and the third label to generate subplot rows, as needed. Technically applicable to any dataframe that contains
 an index level named "iteration" if the other comparison indices are specified.
-
-'t-sne': Generates 2-D embeddings of all high-dimensional data in the concerned set using the same embedding space,
-then plots a comparison of the embeddings of the individual datasets, as identified by the given indices. Accepts
-either 1 or 2 index names for drawing comparisons across subplot columns and rows respectively. Accepts an additional,
-optional keyword argument to specify a depth-coloring metric for the embedded data points.
 """
 
 
@@ -29,7 +24,6 @@ sns.set_palette('tab10')
 sns.set_context("paper", font_scale=2.5)
 
 fixed_metrics_row_index_labels: Sequence[str] = ("model", "metric", "rng_offset", "iteration")
-fixed_runhistory_row_index_labels: Sequence[str] = ("model", "rng_offset", "iteration")
 
 def _mean_std_plot(ax: plt.Axes, data: pd.DataFrame, across: str):
     """ Plots a Mean-Variance metric data visualization on the given Axes object comparing all indices defined by the
@@ -54,8 +48,6 @@ def _mean_std_plot(ax: plt.Axes, data: pd.DataFrame, across: str):
 def mean_std(data: pd.DataFrame, indices: List[str] = None, save_data: bool = True, output_dir: Path = None,
              suptitle:str = None):
     index: pd.MultiIndex = data.index
-    ncols = 1
-    nrows = 1
 
     if save_data:
         if output_dir is None:
@@ -69,19 +61,26 @@ def mean_std(data: pd.DataFrame, indices: List[str] = None, save_data: bool = Tr
     # Identify the requested layout
     nind = len(indices)
     assert nind <= 3, "Mean-Variance visualization of metric values cannot handle more than 3 " \
-                              "index names to compare across."
-    idx3 = idx2 = idx1 = None
+                      "index names to compare across."
+    idx3 = idx2 = None
     row_labels: Sequence[Any] = [None]
     col_labels: Sequence[Any] = [None]
     if nind > 2:
         idx3: str = indices[2]
-        nrows = len(index.get_level_values(idx3).unique())
+        assert idx3 in index.names, f"{idx3} is not a valid level name for the given dataframe with levels " \
+                                    f"{index.names}"
         row_labels: Sequence[Any] = index.unique(level=idx3)
     if nind > 1:
         idx2: str = indices[1]
-        ncols = len(index.get_level_values(idx2).unique())
+        assert idx2 in index.names, f"{idx2} is not a valid level name for the given dataframe with levels " \
+                                    f"{index.names}"
         col_labels: Sequence[Any] = index.unique(level=idx2)
+
     idx1: str = indices[0]
+    assert idx1 in index.names, f"{idx1} is not a valid level name for the given dataframe with levels " \
+                                f"{index.names}"
+    nrows = len(row_labels)
+    ncols = len(col_labels)
 
     def get_view_on_data(row_val, col_val) -> pd.DataFrame:
         """ Returns a cross-section of the full dataframe index using the given values of the comparison indices. """
