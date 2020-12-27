3d93d1f407d215848fa1f51beb9a681d7bc176b3
Archit Bansal <archit.bansal.93@gmail.com>
2020-12-27 23:16:52 +0100

    Fixed and tested t-SNE visualization generation.
    Signed-off-by: Archit Bansal <archit.bansal.93@gmail.com>

--- pybnn/analysis_and_visualization_tools/visualizations/runhistory.py
+++ pybnn/analysis_and_visualization_tools/visualizations/runhistory.py
@@ -10,40 +10,45 @@ optional keyword argument to specify a depth-coloring metric for the embedded da
 
 
 import logging
-from typing import List, Sequence, Any
+from typing import Tuple, List, Sequence, Any, Optional
 from pathlib import Path
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.ticker as mtick
 import pandas as pd
 import numpy as np
+import math
 
 _log = logging.getLogger(__name__)
 # Instead of importing these values from BenchmarkData, they are intentionally re-written here to prevent and detect
 # version mismatch errors.
-fixed_runhistory_row_index_labels: Sequence[str] = ("model", "rng_offset", "iteration")
-y_value_label = 'objective_value'
-runhistory_data_level_name = 'run_data'
-tsne_data_col_labels = ['dim1', 'dim2', y_value_label]
-tsne_data_level_name = 'tsne_data'
+_fixed_runhistory_row_index_labels: Sequence[str] = ("model", "rng_offset", "iteration")
+_y_value_label = 'objective_value'
+_runhistory_data_level_name = 'run_data'
+_tsne_data_col_labels = ['dim1', 'dim2', _y_value_label]
+_tsne_data_level_name = 'tsne_data'
+_diversity = 2048
+_palettes = ['RdYlBu', 'RdBu', 'Spectral', 'coolwarm_r', 'RdYlGn', 'bwr',
+             'seismic', 'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy']
 
 def _initialize_seaborn():
     """ Since seaborn can be finicky on the server, we only import it when we're really sure about it. """
 
     import seaborn as sns
 
-    sns.set_style("whitegrid")
+    sns.set_style("darkgrid")
     sns.set_palette('tab10')
     sns.set_context("paper", font_scale=2.5)
     return sns
 
 
-def perform_tsne(data: pd.DataFrame, save_data: bool = True, output_dir: Path = None) -> pd.DataFrame:
+def perform_tsne(data: pd.DataFrame, save_data: bool = True, output_dir: Path = None,
+                 filename: str = "tsne_embeddings") -> pd.DataFrame:
     """ Given a runhistory dataframe, generates TSNE embeddings in 2 dimensions for the data and returns the embedded
     data as a dataframe with the same index as the runhistory dataframe.
 
     The DataFrame itself should conform to these conditions:
-    Row Index: Should be the Multi-Index with names defined in fixed_runhistory_row_index_labels, such that all values
+    Row Index: Should be the Multi-Index with names defined in _fixed_runhistory_row_index_labels, such that all values
     up to and including index "0" of the level "iteration" correspond to random samples and will be excluded from the
     t-SNE projection. Including these samples would pollute the embedding since they will attach an extremely high
     probability score to the random samples, and we are mostly only interested in the differences between the model
@@ -58,39 +63,45 @@ def perform_tsne(data: pd.DataFrame, save_data: bool = True, output_dir: Path = 
             output_dir = Path().cwd()
         output_dir.mkdir(parents=True, exist_ok=True)
 
-    assert data.columns.nlevels == 1 and data.columns.names == (runhistory_data_level_name,), \
+    assert data.columns.nlevels == 1 and data.columns.names == (_runhistory_data_level_name,), \
         f"The DataFrame 'data' should have a 1-level column index containing only the level name " \
-        f"{runhistory_data_level_name}, was instead {data.columns.names} containing {data.columns.nlevels} levels."
+        f"{_runhistory_data_level_name}, was instead {data.columns.names} containing {data.columns.nlevels} levels."
 
     from sklearn.manifold import TSNE
-    config_dims = data.columns.drop(y_value_label)
+    config_dims = data.columns.drop(_y_value_label)
     # Get rid of random samples
-    configs = data.xs(np.s_[1:], level=fixed_runhistory_row_index_labels[-1], drop_level=False)
+    configs = data.xs(np.s_[1:], level=_fixed_runhistory_row_index_labels[-1], drop_level=False)
     # Get rid of NaN values
     configs = configs[configs.notna().any(axis=1)]
     tsne = TSNE(n_components=2, n_jobs=1)
     # Perform t-SNE transformation on only the x-values
     tsne_data = tsne.fit_transform(configs.loc[pd.IndexSlice[:], config_dims].to_numpy())
     # Append y-values to configuration embeddings
-    y_values = configs.loc[pd.IndexSlice[:], y_value_label]
+    y_values = configs.loc[pd.IndexSlice[:], _y_value_label]
     if tsne_data.shape[0] != y_values.shape[0]:
         raise RuntimeError("There is a mismatch in the number of data points mapped by t-SNE and the number of data "
                            "points expected.")
     tsne_data = np.concatenate((tsne_data, y_values.to_numpy().reshape(-1, 1)), axis=1)
     # Re-package the t-SNE embeddings into a DataFrame
-    tsne_cols = pd.Index(data=tsne_data_col_labels, name=tsne_data_level_name)
+    tsne_cols = pd.Index(data=_tsne_data_col_labels, name=_tsne_data_level_name)
     tsne_df = pd.DataFrame(data=tsne_data, index=configs.index, columns=tsne_cols)
 
     if save_data:
-        tsne_df.to_pickle(output_dir / "tsne_embeddings.pkl.gz")
+        tsne_df.to_pickle(output_dir / f"{filename}.pkl.gz")
 
     return tsne_df
 
 
-def plot_embeddings(embedded_data: pd.DataFrame, indices: List[str], save_data: bool = True, output_dir: Path = None,
-             suptitle:str = None) -> plt.Figure:
+def plot_embeddings(embedded_data: pd.DataFrame, indices: Tuple[List[str], List[str]], save_data: bool = True,
+                    output_dir: Path = None, suptitle: str = None) -> plt.Figure:
     """ Given a dataframe containing t-SNE embeddings in 2 dimensions and up to 2 strings specifying indices across
-    which comparisons are to be generated, creates a figure containing all the relevant plots of the embeddings. """
+    which comparisons are to be generated, creates a figure containing all the relevant plots of the embeddings. The
+    indices can be specified as a tuple of lists of strings such that the total number of strings in both lists
+    combined can be, at most, 2. The first list indicates indices from the row-index and the second list indicates
+    indices from the column-index of the DataFrame (not the subplots). If no column indices are specified, it is
+    expected that the column index has only 1 level. All other levels in the row index than the ones specified in
+    'indices' get reduced. Note that the column index level 'run_data' is reserved and may produce unexpected results
+    if included in 'indices'. """
 
     sns = _initialize_seaborn()
 
@@ -100,39 +111,58 @@ def plot_embeddings(embedded_data: pd.DataFrame, indices: List[str], save_data: 
         output_dir.mkdir(parents=True, exist_ok=True)
 
     if not indices:
-        # Use the default values
-        indices = fixed_runhistory_row_index_labels[:2]
+        # Use the default values, which places a single model in every subplot column.
+        indices = (_fixed_runhistory_row_index_labels[:1], None)
 
-    # Identify the requested layout
-    nind = len(indices)
-    assert nind <= 2, "t-SNE embedding visualization of runhistory cannot handle more than 2 index names to compare " \
-                      "across."
-    idx2 = None
+    df_row_indices, df_col_indices = indices
+    if not df_row_indices:
+        df_row_indices = []
+    if not df_col_indices:
+        df_col_indices = []
+
+    # ########### Identify the requested layout of the plot ########################################################## #
+    nind = len(df_row_indices) + len(df_col_indices)
+    assert nind <= 2, \
+        f"Cannot generate t-SNE comparison across more than 2 indices, received indices: {indices}"
+
+    indices = tuple((idx, 0) for idx in df_row_indices) + tuple((idx, 1) for idx in df_col_indices)
+
+    col_label_level_name: str = indices[0][0]  # The level name in the Multi-Index to be used for column labels.
+    col_labels_axis: int = indices[0][1]  # Which axis (rows or columns) of the DataFrame to use for column labels.
+    assert col_label_level_name in embedded_data.axes[col_labels_axis].names, \
+        f"{col_label_level_name} is not a valid level name for the given dataframe with levels " \
+        f"{embedded_data.axes[col_labels_axis].names}"
+    col_labels: Sequence[Any] = embedded_data.axes[col_labels_axis].unique(level=col_label_level_name)
+
+    row_label_level_name: str = None
     row_labels: Sequence[Any] = [None]
     if nind > 1:
-        idx2: str = indices[1]
-        assert idx2 in embedded_data.index.names, f"{idx2} is not a valid level name for the given dataframe with " \
-                                                  f"levels {embedded_data.index.names}"
-        row_labels: Sequence[Any] = embedded_data.index.unique(level=idx2)
+        row_label_level_name: str = indices[1][0]   # The level name in the Multi-Index to be used for row labels.
+        row_labels_axis: int = indices[1][1]    # Which axis (rows or columns) of the DataFrame to use for row labels.
+        assert row_label_level_name in embedded_data.axes[row_labels_axis].names, \
+            f"{row_label_level_name} is not a valid level name for the given dataframe with levels " \
+            f"{embedded_data.axes[row_labels_axis].names}"
+        row_labels: Sequence[Any] = embedded_data.axes[row_labels_axis].unique(level=row_label_level_name)
 
-    idx1: str = indices[0]
-    assert idx1 in embedded_data.index.names, f"{idx1} is not a valid level name for the given dataframe with " \
-                                              f"levels {embedded_data.index.names}"
-    col_labels: Sequence[Any] = embedded_data.index.unique(level=idx1)
     nrows = len(row_labels)
     ncols = len(col_labels)
+    # ################################################################################################################ #
 
-    def get_view_on_data(row_val, col_val) -> pd.DataFrame:
-        """ Returns a cross-section of the full dataframe index using the given values of the comparison indices. """
+    def get_view_on_data(row_val: Optional[Any] = None, col_val: Optional[Any] = None) \
+            -> pd.DataFrame:
+        """ Returns a cross-section of the full dataframe index using the given values of the comparison indices. If no
+        'col_val' is given, the view includes the entire DataFrame. Otherwise, a cross-section over the 'row_val' and
+        'col_val' is generated. If 'row_val' is None, it is ignored. """
 
-        nonlocal embedded_data
-        if col_val is None:
-            return embedded_data
-        if row_val is None:
-            selection = col_val, idx1
-        else:
-            selection = (row_val, col_val), (idx2, idx1)
-        return embedded_data.xs(selection[0], level=selection[1])
+        nonlocal embedded_data, row_labels_axis, row_label_level_name, col_labels_axis, col_label_level_name
+        selection = embedded_data
+        if col_val is not None:
+            selection = selection.xs(col_val, axis=col_labels_axis, level=col_label_level_name, drop_level=False)
+            if row_val is not None:
+                selection = selection.xs(row_val, axis=row_labels_axis, level=row_label_level_name, drop_level=False)
+        # Since some rows for particular columns of the embedded DataFrame can contain NaNs, we get rid of them from
+        # the view.
+        return selection[selection.notna().all(axis=1)]
 
     # 10% padding in each dimension between axes, each axes object of size (6.4, 4.8), additional 10% padding around the
     # figure edges.
@@ -140,27 +170,31 @@ def plot_embeddings(embedded_data: pd.DataFrame, indices: List[str], save_data: 
     fig, axes = plt.subplots(nrows, ncols, squeeze=False, frameon=True)
     fig: plt.Figure
     axes: np.ndarray
-    colours = enumerate(sns.color_palette(as_cmap=True))
+
+
     for ridx, rlabel in enumerate(row_labels):
+        palettes = enumerate(_palettes)
         for cidx, clabel in enumerate(col_labels):
             ax: plt.Axes = axes[ridx, cidx]
             view = get_view_on_data(row_val=rlabel, col_val=clabel)
-            xs = view.loc[:, view.columns[0]].to_numpy()
-            ys = view.loc[:, view.columns[1]].to_numpy()
-            cs: np.ndarray = view.loc[:, view.columns[2]].to_numpy()
+            if isinstance(view.columns, pd.MultiIndex):
+                data = view.xs(slice(None), axis=1, level=_tsne_data_level_name, drop_level=False)
+                data: np.ndarray = data.to_numpy()
+            else:
+                # view.columns is an object of type Index, assumed to be of the correct type.
+                data: np.ndarray = view.to_numpy()
+            xs = data[:, 0].reshape(-1)
+            ys = data[:, 1].reshape(-1)
+            cs = data[:, 2].reshape(-1)
 
-            # We generate normalize to a log scale, effectively producing the greatest diversity for the lowest values,
-            # which is the region that interests us more since we want to minimize the objective function.
-            norm = mcolors.LogNorm(vmin=np.min(cs), vmax=np.max(cs))
+            # norm = mcolors.Normalize()
+            norm = mcolors.SymLogNorm(linthresh=1e-2, base=10)
             # A complicated but necessary procedure to convert our alphas into a colormap. This makes it easy to create
             # a colorbar later on.
-            ccount, c = np.full(shape=(cs.shape[0], 3), fill_value=next(colours))
-            # Generate as many individual log-scale alpha samples as there are data points. Overkill, but works.
-            # We also invert it, so the smallest objective values will reside on alpha=1.0.
-            alphas = np.linspace([0.], [1.], cs.shape[0])[::-1]   # Produces a column vector
-            cmap = mcolors.ListedColormap(colors=np.concatenate((c, alphas), axis=1), name=f"cmap_{ccount}")
+            ccount, palette = next(palettes)
+            cmap = sns.color_palette(palette, as_cmap=True)
             sc = ax.scatter(xs, ys, c=cs, cmap=cmap, norm=norm)
-            fig.colorbar(sc, ax=ax)
+            fig.colorbar(sc, ax=ax, extend='max')
 
             if ridx == nrows - 1:
                 ax.set_xlabel(clabel, labelpad=10)
